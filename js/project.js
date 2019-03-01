440419318df5ed96bb70931ccbeac9f8b93f8cf3
Hayden @ School <admin@dev.dance>
2019-03-01 14:25:47 -0500

    Hopefully final commit?

--- js/project.js
+++ js/project.js
@@ -13,6 +13,16 @@ function scrollToContact() {
 $(document).ready(function() {
   $("body").css("display", "none");
   $("body").fadeIn(1000);
+  var popup = document.getElementById("myPopup");
+  setTimeout(startPopup, 1000);
 });
-
+function startPopup() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+function popupMessage() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("hide");
+  setTimeout(startPopup, 1000);
+}
 //Thanks for reading this. You made my day.
