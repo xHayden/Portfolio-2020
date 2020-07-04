4d39593f5f3e4c373cdf2d60f90b07f26f02f25a
xHayden <93haydcarp@csdecatur.net>
2020-07-03 23:06:16 -0400

    Update and typo fixes

--- js/portfolio.js
+++ js/portfolio.js
@@ -11,19 +11,49 @@ async function changeWordCool(original, newWord){
     for(let i = 1; i <= newWord.length; i++){
         var cropped = newWord.slice(0, i);
         $("#me-name").text(cropped);
-        await sleep(100);
+        await sleep(80);
     }
 }
 
-setTimeout(async () => {
-    let original = "Hayden Carpenter"
-    let newWords = ["A Programmer", "Someone who hates CSS", "A Web Developer", "An Epic Gamer", "A Scissor-Lift Enthusiast", "A Student"]
-    random = Math.floor(Math.random() * newWords.length);
-    changeWordCool(original, newWords[random])
+
+function startNameChange(){
+    setTimeout(async () => {
+        let original = "Hayden Carpenter"
+        let newWords = ["A Programmer", "Someone who hates CSS", "A Web Developer", "An Epic Gamer", "A Scissor-Lift Enthusiast", "A Student"]
+        random = Math.floor(Math.random() * newWords.length);
+        changeWordCool(original, newWords[random])
+     }, 2000);
+}
+
+
+
+$(document).ready(function(){
     
+    let sjOffset = $("#ScoutJanssen").offset().top - 200;
+    console.log(sjOffset)
+    let titleOffset = $("#title").offset().top;
+    let nameTriggered = false;
+    let sjTriggered = false;
+    $("#visit-FRC").css({'margin-left': $(window).width()})
+    
+    if(!nameTriggered){
+        startNameChange()
+        nameTriggered = true;
+    }
+    var $w = $(window).scroll(function(){
+        $('#progress').css({'height': (($w.scrollTop())/($(document).height()- $(window).height())) * $(window).height()})
+
+        if(!sjTriggered){
+            if ($w.scrollTop() >= sjOffset){
+                $("#visit-FRC").stop().animate({'margin-left': '0'}, 1000, 'linear');
+                sjTriggered = true;
+        
+            } else {
+                // ...
+            }
+        }
+        
+    });
+});
 
 
- }, 2000);
-/*$(document).ready(function(){
-    $('#demo').text('The replaced text.');
-});*/
