52368e70afd27db027d0b82b9bdd235054fbe3fc
xHayden <93haydcarp@csdecatur.net>
2020-07-06 00:32:13 -0400

    added Twitch

--- js/portfolio.js
+++ js/portfolio.js
@@ -109,3 +109,19 @@ $(document).ready(function(){
 });
 
 
+function toggleDropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      let dropdowns = document.getElementsByClassName("dropdown-content");
+      for (let i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
